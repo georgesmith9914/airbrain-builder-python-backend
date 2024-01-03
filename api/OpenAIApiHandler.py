c370498c7154285344471ae7ed399847d5b180a0
GEORGE_SMITH <georgesmith9914@gmail.com>
2024-01-03 22:24:40 +0000

    Added POST method

--- api/OpenAIApiHandler.py
+++ api/OpenAIApiHandler.py
@@ -1,16 +1,31 @@
 import os
-
+import openai
 from flask_restful import Api, Resource, reqparse
 
 
-import openai
-from langchain import OpenAI
-from langchain.prompts import PromptTemplate
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    HumanMessagePromptTemplate,
+    SystemMessagePromptTemplate,
+)
+from langchain.schema import HumanMessage, SystemMessage
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
 
-llm = OpenAI(temperature=0, verbose=True)
+template = (
+    "{persona}."
+)
+system_message_prompt = SystemMessagePromptTemplate.from_template(template)
+human_template = "{text}"
+human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
+
+chat_prompt = ChatPromptTemplate.from_messages(
+    [system_message_prompt, human_message_prompt]
+)
+
 
 class OpenAIApiHandler(Resource):
   # def get(self, place):
@@ -33,10 +48,18 @@ class OpenAIApiHandler(Resource):
     # question = prompt.format(place=place)
     # split() is used to split the items into a list. The llm response will look like:
     # "\n\n1. <first item>.\n\n2. <second item>..."
+    output = llm(
+    chat_prompt.format_prompt(
+        persona="You are a game expert.", text=args.question + "  . Try to limit answer in 1-2 sentences."
+      ).to_messages()
+    )
+
+    print(output)
+
     return {
       'resultStatus': 'SUCCESS',
       #'message': llm(question).split("\n\n")
-      'message': ""
+      'message': output.content
       }
 
   
