af70a5d432babac57b51aad793c8375a21c314b4
GEORGE_SMITH <georgesmith9914@gmail.com>
2024-01-30 23:18:00 +0000

    Updated

--- api/OpenAIApiHandler.py
+++ api/OpenAIApiHandler.py
@@ -1,6 +1,7 @@
 import os
 import openai
 import json
+import yaml
 from flask_restful import Api, Resource, reqparse
 from pysondb import getDb
 from langchain.chat_models import ChatOpenAI
@@ -10,6 +11,8 @@ from langchain.prompts.chat import (
     SystemMessagePromptTemplate,
 )
 from langchain.schema import HumanMessage, SystemMessage
+from dotmap import DotMap
+from benedict import benedict
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
@@ -37,6 +40,7 @@ class OpenAIApiHandler(Resource):
     parser = reqparse.RequestParser()
     parser.add_argument('agent_uuid', type=str)
     parser.add_argument('agent_flow', type=str)
+    parser.add_argument('agent_name', type=str)
     parser.add_argument('question', type=str)
     #parser.add_argument('agent_details', type=str)
     parser.add_argument('agent_telegram_bot_id', type=str)
@@ -55,11 +59,28 @@ class OpenAIApiHandler(Resource):
        else:
           #agent_details = OpenAIApiHandler.getAgentDetails(args.agent_uuid) 
           print("In local else")
+          agent_details = OpenAIApiHandler.getAgentDetails(args.agent_uuid)
+          invalid_json = json.dumps(agent_details)
+          valid_json =  invalid_json.replace("\'", "\"")
+          valid_json =  valid_json.replace('"{', '{')
+          valid_json =  valid_json.replace('}"', '}')
+          print(valid_json)  
+          print("agent persona is ")
+          persona = "" 
+          for agent in json.loads(valid_json):
+             nodes = agent["agent_flow"]["nodes"]
+             for node in nodes:
+                type = node["type"]
+                label = node["data"]["label"]
+                if(type == "personanode"):
+                   persona = label
+          print(persona)   
+
 
        output = llm(
         chat_prompt.format_prompt(
-            persona="You are " + agent_details["agent_persona"], text=args.question + "  . Try to limit answer in 1-2 sentences."
-          ).to_messages()
+            persona="You are " + persona, text=args.question + "  . Try to limit answer in 1-2 sentences."
+         ).to_messages()
         )
 
        print(output)
@@ -68,14 +89,22 @@ class OpenAIApiHandler(Resource):
           'resultStatus': 'SUCCESS',
           #'message': llm(question).split("\n\n")
           'message': output.content
+          #'message': ""
           }
     else:
        print("set agent details")
        agent_details = {
           "agent_uuid": args.agent_uuid,
-          "agent_flow": args.agent_flow
+          "agent_flow": json.loads(json.dumps(args.agent_flow)),
+          "agent_name": args.agent_name
        }
-       OpenAIApiHandler.setAgentDetails(agent_details)
+       saved_item_id = OpenAIApiHandler.setAgentDetails(agent_details)
+       return {
+          'resultStatus': 'SUCCESS',
+          #'message': llm(question).split("\n\n")
+          'message': "flow saved with item id: " + str(saved_item_id)
+          }
+
     # higher temperature leads to more variation, randomness and creativity
     # temperature between 0.7 and 0.9 is most commonly used if you want to experiment and create many variations quickly
     # llm = OpenAI(temperature=0.9)
@@ -90,10 +119,10 @@ class OpenAIApiHandler(Resource):
 
   
   def getAgentDetails(agent_uuid):
-    for item in OpenAIApiHandler.agent_details:
-        if item["agent_uuid"] == agent_uuid:
-           return item
-
+    q = {"agent_uuid": agent_uuid}
+    data = airbrain_db.getByQuery(query=q)
+    return data
+  
   def getAgentDetailsforTelegramBot(agent_telegram_bot_id):
     for item in OpenAIApiHandler.agent_details:
         if item["agent_telegram_bot_id"] == agent_telegram_bot_id:
@@ -116,3 +145,4 @@ class OpenAIApiHandler(Resource):
        print("record does not exist")
        item_id = airbrain_db.add(agent_details)
        print(item_id)
+       return item_id  
